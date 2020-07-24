b5d926559368b9aaac7723abb381464ead892483
carndog <jason.carney1@gmail.com>
2020-07-24 10:49:14 +0100

    Add StatusController

---
+++ Info/Controllers/StatusController.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Info.Controllers
+{
+    public class StatusController : ApiController
+    {
+        public async Task<IHttpActionResult> GetStatusAsync()
+        {
+            return await Task.FromResult(Ok("Hello"));
+        }
+    }
+}
